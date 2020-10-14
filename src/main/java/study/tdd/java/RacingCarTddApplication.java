a8b355543e000698f3da1746bec2904c62c36b9f
pjhsk113 <pjhsk113@naver.com>
2020-10-14 20:39:08 +0900

    project init

---
+++ src/main/java/study/tdd/java/RacingCarTddApplication.java
@@ -0,0 +1,13 @@
+package study.tdd.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacingCarTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RacingCarTddApplication.class, args);
+    }
+
+}
