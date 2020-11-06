97bf2b39225a9de27b0765e57f42c3f7a3eafbc9
pjhsk113 <pjhsk113@naver.com>
2020-11-06 23:17:33 +0900

    [feat] Car 전진 메서드 추가
    - validation 추가

--- src/main/java/study/tdd/java/domain/Car.java
+++ src/main/java/study/tdd/java/domain/Car.java
@@ -1,15 +1,36 @@
 package study.tdd.java.domain;
 
+import study.tdd.java.utils.MoveStrategy;
+
 /**
  * Car 객체에서는 각 자동차의 이동 전략과
  * 이름에 대한 책임을 가진다.
  */
 public class Car {
     private int position;
-    private String name;
+    private final String name;
 
-    public Car(String name) {
+    private Car(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 5자를 초과할 수 없습니다.");
+        }
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("공백은 입력할 수 없습니다.");
+        }
+        if (name == null) {
+            throw new NullPointerException("Null은 입력할 수 없습니다");
+        }
         this.name = name;
+    }
+
+    public static Car from(String name) {
+        return new Car(name);
+    }
+
+    public void move(MoveStrategy moveAble) {
+        if (moveAble.moveAble()) {
+            position++;
+        }
     }
 
     public int getPosition() {
