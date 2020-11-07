fe0c034528ce16b10d96607ffbcff64c27a2419e
pjhsk113 <pjhsk113@naver.com>
2020-11-07 17:00:08 +0900

    [feat] Car 객체 Exception 메시지 & 매직 넘버 상수화
    - validation 메서드 추가

--- src/main/java/study/tdd/java/domain/Car.java
+++ src/main/java/study/tdd/java/domain/Car.java
@@ -7,19 +7,16 @@ import study.tdd.java.utils.MoveStrategy;
  * 이름에 대한 책임을 가진다.
  */
 public class Car {
+    private static final String NAME_LENGTH_EXCEPTION_MESSAGE = "이름은 5자를 초과할 수 없습니다.";
+    private static final String NAME_EMPTY_EXCEPTION_MESSAGE = "공백은 입력할 수 없습니다.";
+    private static final String NAME_NULL_EXCEPTION_MESSAGE = "Null은 입력할 수 없습니다";
+    private static final int NAME_LENGTH_LIMIT = 5;
+
     private int position;
     private final String name;
 
     private Car(String name) {
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("이름은 5자를 초과할 수 없습니다.");
-        }
-        if (name.trim().isEmpty()) {
-            throw new IllegalArgumentException("공백은 입력할 수 없습니다.");
-        }
-        if (name == null) {
-            throw new NullPointerException("Null은 입력할 수 없습니다");
-        }
+        validate(name);
         this.name = name;
     }
 
@@ -30,6 +27,28 @@ public class Car {
     public void move(MoveStrategy moveAble) {
         if (moveAble.moveAble()) {
             position++;
+        }
+    }
+
+    private void validate(String name) {
+        checkNameLength(name);
+        isEmpty(name);
+        isNull(name);
+    }
+
+    private void checkNameLength(String name) {
+        if (name.length() > NAME_LENGTH_LIMIT) {
+            throw new IllegalArgumentException(NAME_LENGTH_EXCEPTION_MESSAGE);
+        }
+    }
+    private void isEmpty(String name) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException(NAME_EMPTY_EXCEPTION_MESSAGE);
+        }
+    }
+    private void isNull(String name) {
+        if (name == null) {
+            throw new NullPointerException(NAME_NULL_EXCEPTION_MESSAGE);
         }
     }
 
