16204e1a9f592c1523b6354ba9e1023247d1d500
pjhsk113 <pjhsk113@naver.com>
2020-11-10 22:02:42 +0900

    [refactor] 테스트용 이동 전략 수정

--- src/main/java/study/tdd/java/domain/TestMoveStrategy.java
+++ src/main/java/study/tdd/java/domain/TestMoveStrategy.java
@@ -1,8 +1,19 @@
 package study.tdd.java.domain;
 
 public class TestMoveStrategy implements MoveStrategy{
+    private int number;
+
     @Override
     public boolean moveAble() {
+        boolean isBiggerThanFour = moveNumber(number);
+        if (isBiggerThanFour) {
+            return true;
+        }
         return false;
+    }
+
+    public boolean moveNumber(int number) {
+        this.number = number;
+        return number >= 4;
     }
 }
