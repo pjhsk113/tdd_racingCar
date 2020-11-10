2f2d23863822afa0bfb453053803eb5938b82d00
pjhsk113 <pjhsk113@naver.com>
2020-11-10 22:04:38 +0900

    [feat] Input 추가

---
+++ src/main/java/study/tdd/java/view/Input.java
@@ -0,0 +1,24 @@
+package study.tdd.java.view;
+
+import java.util.Scanner;
+
+import static study.tdd.java.utils.StringUtils.splitName;
+
+public class Input {
+    private static final String INPUT_NAME_MESSAGE = "자동차 이름 입력";
+    private static final String INPUT_ROUND_MESSAGE = "게임 횟수";
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public String[] inputCarName() {
+        System.out.println(INPUT_NAME_MESSAGE);
+        return splitName(scanner.nextLine());
+    }
+
+    public int inputRound() {
+        System.out.println(INPUT_ROUND_MESSAGE);
+        return scanner.nextInt();
+    }
+
+
+}
