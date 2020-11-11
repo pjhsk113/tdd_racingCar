d195a7747ac83bf16233b319cc030338923bc0a8
pjhsk113 <pjhsk113@naver.com>
2020-11-11 21:58:04 +0900

    [feat] 자동차 경기 시작!

--- src/main/java/study/tdd/java/RacingCarTddApplication.java
+++ src/main/java/study/tdd/java/RacingCarTddApplication.java
@@ -1,12 +1,12 @@
 package study.tdd.java;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import study.tdd.java.domain.RacingGame;
 
 @SpringBootApplication
 public class RacingCarTddApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RacingCarTddApplication.class, args);
+        new RacingGame().play();
     }
 }
