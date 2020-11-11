9fbd6737d3d35a4899c113386c50204feb560ccf
pjhsk113 <pjhsk113@naver.com>
2020-11-11 21:46:58 +0900

    [feat] 결과 처리하는 Result 클래스 추가

---
+++ src/main/java/study/tdd/java/view/Result.java
@@ -0,0 +1,36 @@
+package study.tdd.java.view;
+
+import study.tdd.java.domain.Car;
+import study.tdd.java.domain.Cars;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Result {
+    private static final String COLON = ":";
+    private static final String HYPHEN = "-";
+    private static final String COMMA = ",";
+    private static final String WINNER_NOTICE_MESSAGE = "가 최종 우승했습니다";
+
+    public static void printResult(Cars cars) {
+        cars.stream()
+                .forEach(car ->
+                        System.out.println(car.getName() + COLON
+                                + printHyphen(car.getPosition())));
+    }
+
+    private static String printHyphen(int position) {
+        return IntStream.rangeClosed(1, position)
+                .mapToObj(s -> HYPHEN)
+                .collect(Collectors.joining(""));
+    }
+
+    public static void printWinner(List<Car> winner) {
+        String winnerNames =
+                winner.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(COMMA));
+        System.out.println(winnerNames + WINNER_NOTICE_MESSAGE);
+    }
+}
