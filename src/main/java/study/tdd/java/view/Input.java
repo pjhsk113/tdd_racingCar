b19ea7c79f5fda23090c3e08e8e866c92372df2c
pjhsk113 <pjhsk113@naver.com>
2020-11-13 20:24:22 +0900

    [change] 입력 메시지 변경

--- src/main/java/study/tdd/java/view/Input.java
+++ src/main/java/study/tdd/java/view/Input.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 import static study.tdd.java.utils.StringUtils.splitName;
 
 public class Input {
-    private static final String INPUT_NAME_MESSAGE = "자동차 이름 입력";
-    private static final String INPUT_ROUND_MESSAGE = "게임 횟수 입력";
+    private static final String INPUT_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String INPUT_ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
 
     private static final Scanner scanner = new Scanner(System.in);
 
