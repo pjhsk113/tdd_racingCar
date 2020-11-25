2a2f151d55cfc9834bc6b3dd659aa45d4ac571d5
pjhsk113 <pjhsk113@naver.com>
2020-11-25 21:36:24 +0900

    [refactor] round input validation 추가

--- src/main/java/study/tdd/java/view/Input.java
+++ src/main/java/study/tdd/java/view/Input.java
@@ -1,5 +1,6 @@
 package study.tdd.java.view;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import static study.tdd.java.utils.StringUtils.splitName;
@@ -7,20 +8,27 @@ import static study.tdd.java.utils.StringUtils.splitName;
 public class Input {
     private static final String INPUT_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String INPUT_ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String INVALID_INPUT_ROUND = "라운드는 숫자만 입력 가능합니다.";
 
     private static final Scanner scanner = new Scanner(System.in);
 
     private Input() { }
 
     public static String[] inputCarName() {
+
         System.out.println(INPUT_NAME_MESSAGE);
         return splitName(scanner.nextLine());
     }
 
     public static int inputRound() {
-        System.out.println(INPUT_ROUND_MESSAGE);
-        return scanner.nextInt();
-    }
+        try {
+            System.out.println(INPUT_ROUND_MESSAGE);
+            int round = scanner.nextInt();
 
+            return round;
+        } catch (InputMismatchException e) {
+            throw new NumberFormatException(INVALID_INPUT_ROUND);
+        }
+    }
 
 }
